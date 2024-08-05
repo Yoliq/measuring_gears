1d26cb627c84a168627dbc817f993987beb3d3e7
Peter-Jail <p.kaderabek@gmail.com>
2024-08-05 21:07:01 +0200

    Funkční endstop

--- BigGUI/build/Endstop.py
+++ BigGUI/build/Endstop.py
@@ -11,22 +11,16 @@ class Endstop:
 
     def _GPIO_setup(self):
         GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.add_event_detect(self.pin, self.pressed_state, callback=self.pressed_action, bouncetime=300)
+        GPIO.add_event_detect(self.pin, GPIO.FALLING, callback=self.pressed_action, bouncetime=1000)
 
-    
-    def pressed_action(self):
+
+    def pressed_action(self, channel):
         # stop motor
         self.motor_to_stop.stop_motor()
         # couvni s motorem
         print("End stop reached!")
-        self.motor_to_stop.stop_motor()
         time.sleep(0.5)
-        cuknuti = self.motor_to_stop.steps / 360 *self.motor_to_stop.prevodovy_pomer * self.motor_to_stop.endstop_velikost_cuknuti
-        GPIO.output(self.motor_to_stop.dir_pin, GPIO.HIGH)
-        for _ in range(int(cuknuti)):
-            GPIO.output(self.motor_to_stop.step_pin, GPIO.HIGH)
-            time.sleep(self.motor_to_stop.speed)
-            GPIO.output(self.motor_to_stop.step_pin, GPIO.LOW)
-            time.sleep(self.motor_to_stop.speed)
-        self.motor_to_stop.motor_running = False
+        
+    
+        self.motor_to_stop.sekvence_up(self.motor_to_stop.endstop_velikost_cuknuti)
             # TODO vynulovat hodnoty pro natoceni paky
