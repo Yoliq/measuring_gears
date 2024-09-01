9072ae2e7a45c09bb695627f928476684eadc88d
Peter-Jail <p.kaderabek@gmail.com>
2024-09-01 23:40:02 +0200

    Upravenej skript pro testovani grafu

--- BigGUI/build/graph_test.py
+++ BigGUI/build/graph_test.py
@@ -1,18 +1,25 @@
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk #zobrazení grafu v tkinteru
 import tkinter as tk
+from tkinter import Tk, Canvas
 import numpy as np
 import pandas as pd
 
 # Initialize Tkinter and Matplotlib Figure
-root = tk.Tk()
-fig, ax = plt.subplots(figsize=(7.04, 4.6), dpi=100) # 704x460 pixelů
- 
-# Tkinter Application
-frame = tk.Frame(root)
-label = tk.Label(text = "Matplotlib + Tkinter!")
-label.config(font=("Courier", 32))
-label.pack()
+window = Tk()
+window.geometry("2560x1440")
+window.configure(bg='gray')
+# Create Canvas
+canvas = Canvas(
+    window,
+    bg="gray",
+    height=1440,
+    width=2560,
+    bd=0,
+    highlightthickness=0,
+    relief="ridge"
+)
+canvas.place(x=0, y=0)
 
 # Nacteni dat z csv
 data_do_grafu = pd.read_csv('/home/pi/Petr/measuring_gears/BigGUI/build/csv/Test_csv_1.csv')
@@ -21,23 +28,20 @@ t = data_do_grafu.iloc[:, 0]
 uhel1 = data_do_grafu.iloc[:, 1]
 uhel2 = data_do_grafu.iloc[:, 2]
 
-# Create Canvas
-canvas = FigureCanvasTkAgg(fig, master=root)  
-canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+fig, ax = plt.subplots(figsize=(7.04, 4.6), dpi=100) # 704x460 pixelů
+graf_canvas = FigureCanvasTkAgg(fig, master=window)  
+graf = graf_canvas.get_tk_widget()
+graf.place(x=1811, y=262, width=704, height=460)
 
-toolbar = NavigationToolbar2Tk(canvas, root, pack_toolbar=False)
+toolbar = NavigationToolbar2Tk(graf_canvas, window, pack_toolbar=False)
 toolbar.update()
-toolbar.pack(anchor="center", fill=tk.X)
+toolbar.place(x=1811, y=722, width=704, height=30)
 
-frame.pack() 
-# Plot data on Matplotlib Figure
-# t = np.arange(0, 2*np.pi, .01)
-# ax.plot(t, np.sin(t))
 ax.plot(t, uhel1, label='Úhel 1')
 ax.plot(t, uhel2, label='Úhel 2')
 ax.set_xlabel('Čas [s]')
 ax.set_ylabel('Úhel [°]')
 ax.legend()
-canvas.draw()
+graf_canvas.draw()
  
-root.mainloop()
+window.mainloop()
